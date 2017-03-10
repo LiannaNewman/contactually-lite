afe7077e7e97207ff7193ff9193e50b524a8d19e
Lianna Newman <liannanewman@gmail.com>
2017-03-10 10:38:23 -0500

    successfully added csv file uploader, updated Contacts controller, Contact Model, ContactsIndex page, routes, and added contactually logo to images file for later use.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,20 @@ class ContactsController < ApplicationController
   def index
     @contacts = Contact.all
   end
+
+  def import
+    Contact.import(params[:file])
+    redirect_to '/contacts', notice: 'Contacts Successfully Uploaded!'
+  end
+
+  def create
+    @contact = Contact.create(
+      contact_id: params[:id],
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email_address: params[:email_address],
+      phone_number: params[:phone_number],
+      company_name: params[:company_name]
+    )
+  end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,10 @@
 class Contact < ApplicationRecord
+  require 'csv'
+  require 'tempfile'
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      Contact.create! row.to_hash
+    end
+  end
 end
